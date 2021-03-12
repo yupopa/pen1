490ebf21552c061d76b16c2dc0957b12b4307fd8
yupopa <61375250+yupopa@users.noreply.github.com>
2021-03-12 22:42:15 +0300

    Update pen.py

--- pen.py
+++ pen.py
@@ -3,8 +3,8 @@ from fastai.vision.all import *
 from pathlib import Path
 import streamlit as st
 from urllib.request import urlretrieve
-url = 'http://dl.dropboxusercontent.com/s/6j8y83elzpenz1c/export.pkl?raw=1'
-filename = 'export.pkl'
+url = 'http://dl.dropboxusercontent.com/s/f54qsk57cs1u9xr/newbest.pkl?raw=1'
+filename = 'newbest.pkl'
 urlretrieve(url,filename)
 st.markdown("HIGHLIGHTER OR PENCIL")
 
